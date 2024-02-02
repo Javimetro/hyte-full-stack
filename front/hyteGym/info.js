ee02c6f03cad42ada3de05c3a4fc560d742d9ed4
javimetro <javierj@metropolia.fi>
2024-02-02 20:05:42 +0200

    front end tehtävät viikko 3 tehty

---
+++ front/hyteGym/info.js
@@ -0,0 +1,31 @@
+window.addEventListener ('load', function() {
+    // Lisää tietoa selaimesta
+    // Data taulukko
+    var target = document.getElementById('target');
+    var lang = navigator.language || navigator.userLanguage;
+    target.innerHTML += '<p>Selaimen kieli: ' + lang + '</p>';
+
+    var screenWidth = window.screen.width;
+    var screenHeight = window.screen.height;
+    target.innerHTML += '<p>Näytön koko: ' + screenWidth + ' x ' + screenHeight + '</p>';
+
+
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    target.innerHTML += '<p> Selaimen ikkunan koko: ' + windowWidth + ' x ' + windowHeight + '</p>';
+
+
+    var date = new Date();
+    var dateString = date.toLocaleDateString('fi-Fi', {day: 'numeric', month: 'long', year: 'numeric'});
+    var timeString = date.toLocaleTimeString('fi-Fi', {hour: '2-digit', minute: '2-digit'});
+    target.innerHTML += '<p>Tänään on ' + dateString + ' ja kello on ' + timeString + '</p>';
+
+    // Lisää overlay kuva
+    var box = document.querySelector('.box');
+    var overlay = document.createElement('img');
+
+    overlay.src = 'images/heart.png';
+    overlay.className = 'overlayImage';
+
+    box.appendChild(overlay)
+});
